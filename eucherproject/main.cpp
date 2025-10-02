8fd78f00e1c2c00ad69eb43591377b246dfbfcf9
Thomas <tommyboyle04502@gmail.com>
2025-10-02 10:11:45 -0400

    Initial Commit

---
+++ eucherproject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  eucherproject
+//
+//  Created by Thomas Boyle on 10/2/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
