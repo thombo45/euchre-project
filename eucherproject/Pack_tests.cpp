a1ee049f25407deb6ca5e7264a3aa7be59c586aa
Drew <drmoses@umich.edu>
2025-10-05 13:58:37 -0400

    add tests to Pack_test.cpp
    added another constructor test to the file

--- eucherproject/Pack_tests.cpp
+++ eucherproject/Pack_tests.cpp
@@ -13,5 +13,38 @@ TEST(test_pack_default_ctor) {
 }
 
 // Add more tests here
+TEST(test_pack_default_ctor_2) {
+    Pack pack;
+    Card first = pack.deal_one();
+    ASSERT_EQUAL(JACK, first.get_rank());
+    ASSERT_EQUAL(HEARTS, first.get_suit());
+
+    Card third = pack.deal_one();
+    ASSERT_EQUAL(TWO, third.get_rank());
+    ASSERT_EQUAL(CLUBS, third.get_suit());
+
+    Card sixth = pack.deal_one();
+    ASSERT_EQUAL(FOUR, sixth.get_rank());
+    ASSERT_EQUAL(DIAMONDS, sixth.get_suit());
+
+    Card eighteenth = pack.deal_one();
+    ASSERT_EQUAL(ACE, eighteenth.get_rank());
+    ASSERT_EQUAL(SPADES, eighteenth.get_suit());
+    
+    Card last = pack.deal_one();
+    ASSERT_EQUAL(TEN, last.get_rank());
+    ASSERT_EQUAL(HEARTS, last.get_suit());
+
+    for (int i = 0; i != 7; ++i) {
+        pack.deal_one();
+    }
+
+    for (int i = 0; i != 19; ++i) {
+        pack.deal_one();
+    }
+    for (int i = 0; i != 25; ++i) {
+        pack.deal_one();
+    }
+}
 
 TEST_MAIN()
