184bbd864b4ff398c1385c03800813071012762e
Drew <drmoses@umich.edu>
2025-10-04 21:56:52 -0400

    Implemented all functions in Pack.cpp but the first one is wrong
    will start over but wanted to push this

---
+++ eucherproject/Pack.cpp
@@ -0,0 +1,88 @@
+#include "Card.hpp"
+#include "Pack.hpp"
+#include <array>
+#include <string>
+
+const int firstCard = 0;
+const int LastCard = 23;
+const int startArray = 7;
+
+
+  // EFFECTS: Initializes the Pack to be in the following standard order:
+  //          the cards of the lowest suit arranged from lowest rank to
+  //          highest rank, followed by the cards of the next lowest suit
+  //          in order from lowest to highest rank, and so on. 
+  // NOTE: The standard order is the same as that in pack.in.
+  // NOTE: Do NOT use pack.in in your implementation of this function
+  // NOTE: The pack is initially full, with no cards dealt.
+  Pack::Pack() {
+    /*next = firstCard;
+    int j = startArray;
+    int k = 0;
+    for (int i = 0; i != PACK_SIZE; ++i) {
+        Card card(Rank(j), k);
+        cards[i] = card;
+        ++j;
+
+        if (j == Rank(13)) {
+            j = startArray;
+            ++k;
+        }
+    }*/
+  }
+
+  // REQUIRES: pack_input contains a representation of a Pack in the
+  //           format required by the project specification
+  // MODIFIES: pack_input
+  // EFFECTS: Initializes Pack by reading from pack_input.
+  // NOTE: The pack is initially full, with no cards dealt.
+  Pack::Pack(std::istream& pack_input) {
+    next = firstCard;
+    std::string rank, suit, of;
+
+    for (int i = 0; i != PACK_SIZE; ++i) {
+        pack_input >> rank >> of >> suit;
+        Card card(Rank rank_in, Suit suit_in);
+        cards[i];
+    }
+  }
+
+  // REQUIRES: cards remain in the Pack
+  // EFFECTS: Returns the next card in the pack and increments the next index
+  Card Pack::deal_one() {
+    return cards[next++];
+  }
+
+  // EFFECTS: Resets next index to first card in the Pack
+  void Pack::reset() {
+    next = firstCard;
+  }
+
+  // EFFECTS: Shuffles the Pack and resets the next index. This
+  //          performs an in shuffle seven times. See
+  //          https://en.wikipedia.org/wiki/In_shuffle.
+  void Pack::shuffle() {
+    int shuffles = 7;
+    std::array <Card, PACK_SIZE / 2> first_half, second_half;
+
+    while (shuffles != 0) {
+        for (int i = 0; i != PACK_SIZE / 2; ++i) {
+            first_half[i] = cards[i];
+            second_half[i] = cards[PACK_SIZE / 2 + i];
+        }
+        for (int i = 0, j = 0; i != PACK_SIZE; i += 2, ++j) {
+            cards[i] = second_half[j];
+            cards[i + 1] = first_half[j];
+        }
+        --shuffles;
+    }
+    next = 0;
+  }
+
+  // EFFECTS: returns true if there are no more cards left in the pack
+  bool Pack::empty() const {
+    if (next == LastCard + 1) {
+        return true;
+    }
+    return false;
+  }
