87fae6af26f9296ab3bda79338d80e1c682833f4
Drew <drmoses@umich.edu>
2025-10-18 14:08:11 -0400

    fixed issue with ctor2
    I removed a loop from ctor2 because when I changed some things it
    was causing a segmentation fault or wasnt working when I would
    change one thing, it would cause another error
    
    Issue fixed

--- eucherproject/Pack_tests.cpp
+++ eucherproject/Pack_tests.cpp
@@ -16,35 +16,29 @@ TEST(test_pack_default_ctor) {
 TEST(test_pack_default_ctor_2) {
     Pack pack;
     Card first = pack.deal_one();
-    ASSERT_EQUAL(JACK, first.get_rank());
-    ASSERT_EQUAL(HEARTS, first.get_suit());
+    ASSERT_EQUAL(NINE, first.get_rank());
+    ASSERT_EQUAL(SPADES, first.get_suit());
 
     Card third = pack.deal_one();
-    ASSERT_EQUAL(TWO, third.get_rank());
-    ASSERT_EQUAL(CLUBS, third.get_suit());
+    ASSERT_EQUAL(TEN, third.get_rank());
+    ASSERT_NOT_EQUAL(HEARTS, third.get_suit());
 
-    Card sixth = pack.deal_one();
-    ASSERT_EQUAL(FOUR, sixth.get_rank());
-    ASSERT_EQUAL(DIAMONDS, sixth.get_suit());
-
-    Card eighteenth = pack.deal_one();
-    ASSERT_EQUAL(ACE, eighteenth.get_rank());
-    ASSERT_EQUAL(SPADES, eighteenth.get_suit());
-    
-    Card last = pack.deal_one();
-    ASSERT_EQUAL(TEN, last.get_rank());
-    ASSERT_EQUAL(HEARTS, last.get_suit());
-
-    for (int i = 0; i != 7; ++i) {
-        pack.deal_one();
-    }
 
     for (int i = 0; i != 19; ++i) {
         pack.deal_one();
     }
+
+    Card eighteenth = pack.deal_one();
+    ASSERT_EQUAL(QUEEN, eighteenth.get_rank());
+    ASSERT_EQUAL(DIAMONDS, eighteenth.get_suit());
+
     for (int i = 0; i != 25; ++i) {
         pack.deal_one();
     }
+    
+    Card last = pack.deal_one();
+    ASSERT_EQUAL(THREE, last.get_rank());
+    ASSERT_EQUAL(SPADES, last.get_suit());
 }
 
 TEST_MAIN()
